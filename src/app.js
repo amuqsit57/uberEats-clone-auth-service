332a4e1840ccbfde033ed002fbf7c26afd73db05
Abdul Muqsit <92204326+amuqsit57@users.noreply.github.com>
2026-04-24 13:52:01 +0500

    add

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ const maskSensitiveFields = (value) => {
 const app = express()
 
 app.use(cors({
-  origin: "http://localhost:3002",
+  origin: "http://localhost:3000",
   credentials: true,
 }));
 
